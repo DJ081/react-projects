408b6ec123dc04b0f7b9f4aabd229a71b57384f9
Draav Jain <draavjain2001@gmail.com>
2022-08-24 11:36:40 +0530

    initial commit

---
+++ 15-cocktails/setup/src/pages/About-LAPTOP-1766P3S0.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, in.
+        Nesciunt totam fuga dolorem quae ipsum veniam perferendis eius
+        doloremque vitae! Esse temporibus ad perferendis facilis voluptas
+        corrupti iste ducimus soluta autem expedita! Qui accusantium molestiae
+        sequi repellat laudantium nostrum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
